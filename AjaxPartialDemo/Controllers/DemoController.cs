35c9f954810114096a362a39123283d71342d866
System Administrator <admin@MngEnvMCAP495792.onmicrosoft.com>
2025-08-01 10:09:08 +0700

    Add project files.

---
+++ AjaxPartialDemo/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxPartialDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(int ID)
+        {
+            // simulate some work
+            ViewBag.Id = ID;
+            return PartialView("~/Views/Shared/_PartialView.cshtml");
+        }
+    }
+}
+
---
+++ AjaxPartialDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AjaxPartialDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
